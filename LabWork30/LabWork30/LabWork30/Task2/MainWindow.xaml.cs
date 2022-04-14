3f7a97d6953e860e19354ba6c0417d33f908ab38
Yamakadzi <102765765+Yamakadzi@users.noreply.github.com>
2022-04-15 00:39:19 +0300

    LabWork30

---
+++ LabWork30/LabWork30/LabWork30/Task2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AgreeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (agreeCheckBox.IsChecked == true)
+            {
+                nomask.Text = mask.Password;
+                mask.Visibility = Visibility.Collapsed;
+                nomask.Visibility = Visibility.Visible;
+            }
+            else 
+            { 
+            mask.Password = nomask.Text;
+            mask.Visibility = Visibility.Visible;
+            nomask.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
